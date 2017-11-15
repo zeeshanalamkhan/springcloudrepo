a4622148a8349e4fd65c3cbb4d3b98ed14824865
Dotridge <dotridge.java@gmail.com>
2017-11-15 15:13:21 +0530

    initial commit

---
+++ patient-service/src/main/java/com/nareshit/PatientServiceApplication.java
@@ -0,0 +1,17 @@
+package com.nareshit;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class PatientServiceApplication{
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+	
+}
---
+++ patient-service/src/main/java/com/nareshit/util/ServiceConstants.java
@@ -0,0 +1,6 @@
+package com.nareshit.util;
+
+public class ServiceConstants {
+
+	public static final String NOVEL_HEALTH_DATE_FORMAT = "novelhealth.date.format";
+}
